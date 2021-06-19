126bc9252c0c54a5b9bb0a67a04803ae2b325c6f
Jean Torres <evinsco@gmail.com>
2021-06-19 18:40:59 +0300

    a

--- src/index.js
+++ src/index.js
@@ -4,21 +4,8 @@ import PrimeraApp from './PrimeraApp';
 
 import './index.css';
 
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
-const divRoot = document.querySelector('#root');
-
-ReactDOM.render(<PrimeraApp saludo="Hola, Soy Goku" />, divRoot);
-
-// ReactDOM.render(
-//    <React.StrictMode>
-//       <App />
-//    </React.StrictMode>,
-//    document.getElementById('root')
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+ReactDOM.render(
+   <App />,
+   document.getElementById('root')
+);
